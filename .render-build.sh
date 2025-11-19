0a796a73c57e69f6610fce46cbf329ffa2ffb62b
RBXSL <sanabriajacob11@gmail.com>
2025-11-19 15:19:43 -0500

    Delete .render-build.sh

--- .render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Install system dependencies needed for aiohttp
-apt-get update
-apt-get install -y build-essential python3-dev libssl-dev libffi-dev
-
-# Install Python dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
